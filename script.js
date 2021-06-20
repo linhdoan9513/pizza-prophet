9814761b5aa8ac2b0917716e6d486db0e7df38a2
linhdoan9513 <doanlinh9513@gmail.com>
2021-06-20 15:34:30 -0400

    Added sorting logic to filter pizza places based on user input.

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ const pizza = {
 //Once form submits, grab values from selected from radio inputs.
 //use Radio input values, to look through pizza object 
 //display the matching pizza joint to user 
+const randomElement = (array) => {
+	const randomIndex = Math.floor(Math.random() * array.length); 
+	return array[randomIndex];
+}
 
 const form = document.querySelector('form');
 form.addEventListener('submit', e => {
@@ -44,5 +48,16 @@ form.addEventListener('submit', e => {
 	//Once form submits, grab values from selected from radio inputs.
 	const sizeSelected = document.querySelector('input[name=size]:checked').value;
 	const priceSelected = document.querySelector('input[name=price]:checked').value;
-	console.log(sizeSelected, priceSelected);
+	//use Radio input values, to look through pizza object 
+	const sizePizzaList = pizza[sizeSelected];
+	const withinPricePizzaList = sizePizzaList.filter((pizzaPlace) => {
+		if (pizzaPlace.price === priceSelected) {
+			return true;
+		}
+		return false; 
+	});
+	const finalPizzaPlace = randomElement(withinPricePizzaList);
+	console.log(finalPizzaPlace);
+	
 })
+
